817557f7d8aa132d967680feb723e9bb4af5ad4c
Sashankr <sashankr97@gmail.com>
2024-01-04 00:36:22 +0530

    updated meta information

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="container mx-auto p-4">
       <h2 className="text-2xl font-medium mb-3">Gym Progress Tracker</h2>
-      <p className="mb-3">Track your gym workouts using this simple app</p>
+      <p className="mb-3">Track your gym workouts using this simple web app.</p>
       <form onSubmit={handleSubmit}>
         <div className="flex flex-col gap-2">
           <label className="font-medium" htmlFor="workout_date">
@@ -17,6 +17,7 @@ export default function App() {
             className="border p-3"
             id="workout_date"
             type="date"
+            max={new Date()}
           />
         </div>
         <button
