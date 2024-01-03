4d9a519731bed0fc1f5bb32ab6c80083a031e814
Sashankr <sashankr97@gmail.com>
2024-01-04 00:49:08 +0530

    basic workout details

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,13 @@
+import { useState } from "react";
+
 export default function App() {
+  const [workoutDate, setWorkoutDate] = useState("");
+
+  const handleWorkoutDateChange = (e) => {
+    const { name, value } = e.target;
+    setWorkoutDate(value);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
   };
@@ -13,11 +22,12 @@ export default function App() {
             Select Workout Date
           </label>
           <input
+            onChange={handleWorkoutDateChange}
             required
             className="border p-3"
             id="workout_date"
             type="date"
-            max={new Date()}
+            value={workoutDate}
           />
         </div>
         <button
@@ -27,6 +37,35 @@ export default function App() {
           Create Workout
         </button>
       </form>
+      {workoutDate !== "" ? (
+        <div className="mt-5">
+          <h2 className="text-lg font-medium underline">
+            Workout Details for {workoutDate}
+          </h2>
+          <div className="p-2 shadow-xl rounded-xl mt-4">
+            <div className="flex flex-col gap-2">
+              <label className="font-medium" htmlFor="workout">
+                Select A Workout
+              </label>
+              <select className="p-3" name="workout" id="workout">
+                <option value="shoulder">Shoulder</option>
+                <option value="back">Back</option>
+                <option value="chest">Chest</option>
+                <option value="bicep_tricep">Bicep + Tricep</option>
+                <option value="legs">Legs</option>
+                <option value="abs">Abs</option>
+              </select>
+            </div>
+            <div className="mt-4">
+              <h3 className="p-2 border-2 border-green-500 cursor-pointer">
+                <span className="mr-2">➕</span> Add Exercice
+              </h3>
+            </div>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
