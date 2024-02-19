1bc6fcc0df7acfe6f36dd1806767c35addb4e6df
Sashankr <sashankr97@gmail.com>
2024-02-19 13:09:58 +0530

    tested api call to save workout to backend

--- src/pages/WorkoutPage/index.jsx
+++ src/pages/WorkoutPage/index.jsx
@@ -7,6 +7,7 @@ import { UPDATE_WEIGHT } from "../../redux/features/weightTrackerSlice";
 const WorkoutPage = () => {
   const dispatch = useDispatch();
   const currentBodyWeight = useSelector((state) => state.weightTracker).weight;
+  const workoutDetails = useSelector((state) => state.workoutDetails);
 
   const navigate = useNavigate();
   const date =
@@ -29,7 +30,7 @@ const WorkoutPage = () => {
         <h2 className="text-lg font-medium underline">
           Adding Workout for {date}
         </h2>
-        <div className="flex flex-col gap-2 w-1/4">
+        <div className="flex w-1/4 flex-col gap-2">
           <label htmlFor="weight">Body Weight ( kg )</label>
           <input
             type="text"
@@ -38,17 +39,17 @@ const WorkoutPage = () => {
               dispatch(UPDATE_WEIGHT(e.target.value));
             }}
             value={currentBodyWeight}
-            className="border-2 px-3 py-2 rounded-lg outline-0 focus:border-blue-950	 active:border-blue-950"
+            className="rounded-lg border-2 px-3 py-2 outline-0 focus:border-blue-950	 active:border-blue-950"
           />
         </div>
-        <div className="p-2 shadow-xl rounded-xl mt-4">
+        <div className="mt-4 rounded-xl p-2 shadow-xl">
           <div className="flex flex-col gap-2">
             <label className="font-medium" htmlFor="workout">
               Select A Workout
             </label>
             <select
               onChange={handleWorkoutChange}
-              className="p-3 border-2 rounded-lg"
+              className="rounded-lg border-2 p-3"
               name="workout"
               id="workout"
             >
@@ -70,18 +71,32 @@ const WorkoutPage = () => {
           ) : (
             ""
           )}
-          <div className="flex justify-end mt-5">
+          <div className="mt-5 flex justify-end">
             <button
-              onClick={() => {
+              onClick={async () => {
                 const answer = confirm(
-                  "Are you sure you want to save this workout?"
+                  "Are you sure you want to save this workout?",
                 );
                 console.log(answer);
                 if (answer) {
-                  navigate("/workout-info");
+                  debugger;
+                  const response = await fetch(
+                    "http://localhost:3001/api/v1/workout/save-workout",
+                    {
+                      method: "POST",
+                      body: JSON.stringify({
+                        bodyWeight: currentBodyWeight,
+                        workoutDetails,
+                      }),
+                      headers: {
+                        "Content-Type": "application/json",
+                      },
+                    },
+                  );
+                  // navigate("/workout-info");
                 }
               }}
-              className="bg-gradient-to-r from-violet-500 to-fuchsia-500  px-3 py-2 transition rounded-lg text-white"
+              className="rounded-lg bg-gradient-to-r from-violet-500  to-fuchsia-500 px-3 py-2 text-white transition"
             >
               Save Workout
             </button>
