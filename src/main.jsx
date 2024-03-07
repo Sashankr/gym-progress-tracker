2e2425d8f46b827d5cd8f54367451895998296b4
Sashankr <sashankr97@gmail.com>
2024-03-07 07:07:00 +0530

    updated response

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import { Provider } from "react-redux";
 import App from "./App.jsx";
 import "./index.css";
 import { store } from "./redux/store.js";
+import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
+      <Toaster position="bottom-center" />
     </Provider>
   </React.StrictMode>
 );
