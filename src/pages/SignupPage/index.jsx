3507f963b53c597cdeab19bc356fbb3d05b2aca3
Sashankr <sashankr97@gmail.com>
2024-03-13 23:59:06 +0530

    signup form

--- src/pages/SignupPage/index.jsx
+++ src/pages/SignupPage/index.jsx
@@ -1,34 +1,163 @@
-import { Input } from "../../components/Input";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
 import { Button } from "../../components/ui/button";
+import { Input } from "../../components/ui/input";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+const formSchema = z.object({
+  username: z
+    .string()
+    .min(2, {
+      message: "Username must be at least 2 characters.",
+    })
+    .max(40, {
+      message: "Username must be at most 40 characters.",
+    })
+    .regex(/^[A-Za-z ]+$/, {
+      message: "Username must contain only letters and spaces.",
+    }),
+  fullName: z
+    .string()
+    .min(2, {
+      message: "Full Name must be at least 2 characters.",
+    })
+    .max(40, {
+      message: "Full Name must be at most 40 characters.",
+    })
+    .regex(/^[A-Za-z ]+$/, {
+      message: "Full Name must contain only letters and spaces.",
+    }),
+  emailId: z.string().email({
+    message: "Invalid email format.",
+  }),
+  password: z.string().max(100, {
+    message: "Password must be at most 100 characters.",
+  }),
+  confirmPassword: z.string().refine((value) => value === formSchema.password, {
+    message: "Passwords do not match.",
+  }),
+});
 
 const SignupPage = () => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      fullName: "",
+    },
+  });
+
+  function onSubmit(values) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
   return (
     <div className="bg-login h-screen bg-cover">
       <div className="signup grid grid-cols-2 h-full">
         <div></div>
-        <div className="bg-black opacity-[0.9] text-white h-full">
+        <div className="bg-black opacity-[0.9]  h-full">
           <div className="flex h-full p-5">
-            <section className="">
-              <h3 className="text-3xl">Gym Progress Tracker</h3>
-              <h3 className="text-3xl mt-5">Signup</h3>
-              <form onSubmit={handleSubmit}>
-                <Input placeholder="Name" className="my-4" />
-                <Input placeholder="Email" type="email" className="my-4" />
-                <Input
-                  placeholder="Password"
-                  type="password"
-                  className="my-4"
-                />
-                <Input
-                  placeholder="Confirm Password"
-                  type="password"
-                  className="my-4"
-                />
-                <Button>Sign Up</Button>
-              </form>
+            <section className="w-full">
+              <h3 className="text-3xl text-white">Gym Progress Tracker</h3>
+              <h3 className="text-3xl mt-5 text-white">Signup</h3>
+              <Form {...form}>
+                <form
+                  onSubmit={form.handleSubmit(onSubmit)}
+                  className="space-y-8"
+                >
+                  <FormField
+                    control={form.control}
+                    name="fullName"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel className="text-white">Full Name</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Enter Full Name" {...field} />
+                        </FormControl>
+
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel className="text-white">Username</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Enter Username" {...field} />
+                        </FormControl>
+
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="emailId"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel className="text-white">Email Id</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Enter Email Id" {...field} />
+                        </FormControl>
+
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel className="text-white">Password</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="Enter Password"
+                            type="password"
+                            {...field}
+                          />
+                        </FormControl>
+
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="confirmPassword"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel className="text-white">
+                          Confirm Password
+                        </FormLabel>
+                        <FormControl>
+                          <Input
+                            type="password"
+                            placeholder="Confirm Password"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <Button type="submit">Submit</Button>
+                </form>
+              </Form>
             </section>
           </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{js,jsx}',
-    './components/**/*.{js,jsx}',
-    './app/**/*.{js,jsx}',
-    './src/**/*.{js,jsx}',
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./app/**/*.{js,jsx}",
+    "./src/**/*.{js,jsx}",
   ],
   prefix: "",
   theme: {
@@ -71,7 +71,10 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      backgroundImage: {
+        login: "url('/login-background.jpg')",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
